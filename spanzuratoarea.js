e003163dcb1a09eb7e0425fbdc429b3971a8c076
Andreea Manole <andreeamanole59@gmail.com>
2023-05-12 14:36:56 +0300

    Adaugare mesaj pentru castig

--- spanzuratoarea.js
+++ spanzuratoarea.js
@@ -5,42 +5,31 @@ const btn = document.getElementById("buton1");
 
 let cuvinte = ["Rinocer", "Taur", "Floare", "Septembrie", "Masinarie", "Avalansa"];
 
+let ghicite = 0;
+
 let maxGreseli = 6;
 
 let letters = document.querySelectorAll('#letter');
 
 paragarf.textContent = `Ai 6 incercari pentru a castiga!`;
 
-
-// document.querySelector('.keywords').onclick = function(event) {
-//     // La fiecare apasare returneaza litera apasata
-//     let a = event.target.innerHTML;
-//     let litere = cuvinte[0].split("")
-//     toUpper = function(litere) {
-//         return litere.toUpperCase()
-//     }
-//     // Transformare litere cuvant, s-ar putea sa nu avem nevoie de el mai incolo
-//     litere = litere.map(toUpper)
-//     if(litere.includes(a)) {
-//         console.log("Ieii")
-//     }
-// }
-
 letters.forEach(letter => {
     letter.addEventListener('click', function(event) {
       let litere = this.textContent.toUpperCase();
       // Verificați dacă litera apăsată există în cuvântul generat
       if (cuvantAles.includes(litere)) {
-        // console.log("Litera există: " + litere);
         // Afișați litera în locul corespunzător în interfața utilizatorului
         let spans = inputSection.getElementsByClassName("dash");
         for (let i = 0; i < cuvantAles.length; i++) {
           if (cuvantAles[i] === litere) {
             spans[i].textContent = litere;
+            ghicite++;
+            if(ghicite == cuvantAles.length){
+              window.alert("Felicitari! Ai castigat!!!");
+            }
           }
         }
       } else {
-        // console.log("Litera nu există: " + litere);
         // Incrementați numărul de greșeli și afișați rezultatul în interfața utilizatorului
         maxGreseli--;
         paragarf.textContent = `Ai gresit! Mai ai ${maxGreseli} incercari.`
