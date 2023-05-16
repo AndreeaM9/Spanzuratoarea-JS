1c56aa716c577bf0deede2deeb7e320d5cceffdf
Mina Iosif Alexandru <minaiosifalexandru@yahoo.com>
2023-05-16 18:00:47 +0300

    Rezolvare mici erori sau scrieri gresite

--- spanzuratoarea.js
+++ spanzuratoarea.js
@@ -2,29 +2,29 @@ const inputSection = document.getElementById("user-input-section");
 const photo = document.getElementById("spanzuratoare");
 const paragraf = document.getElementById("paragraf");
 const btn = document.getElementById("buton1");
-
 let cuvinte = ["Rinocer", "Taur", "Floare", "Septembrie", "Masinarie", "Avalansa"];
-
 let ghicite = 0;
-
 let mesaj = document.getElementById("mesaj");
-
 let maxGreseli = 6;
+let letters = document.querySelectorAll('.letter');
 
-let letters = document.querySelectorAll('#letter');
+// Alegere cuvânt random
+let cuvantAles = cuvinte[Math.floor(Math.random() * cuvinte.length)];
+cuvantAles = cuvantAles.toUpperCase();
 
-paragraf.textContent = `Ai 6 incercari pentru a castiga!`;
+paragraf.textContent = `Ai 6 încercări pentru a câștiga!`;
 
-//Functionalitate buton <Refresh>
+// Functionalitate buton Refresh
 btn.addEventListener("click", () => {
   window.location.reload();
-})
+});
 
 letters.forEach(letter => {
   letter.addEventListener('click', function(event) {
-    //dezactivare litere dupa apasare
+    // Dezactivare litere după apăsare
     letter.setAttribute("disabled", true);
     let litere = this.textContent.toUpperCase();
+
     // Verificați dacă litera apăsată există în cuvântul generat
     if (cuvantAles.includes(litere)) {
       // Afișați litera în locul corespunzător în interfața utilizatorului
@@ -40,57 +40,50 @@ letters.forEach(letter => {
               spans[j].textContent = cuvantAles[j];
             }
             mesaj.textContent = "Felicitări! Ai câștigat!!!";
-            letters.forEach(element => {
-              element.setAttribute("disabled", true);
-            });
             mesaj.style.display = "block";
             onReset();
           }
         }
       }
-      } else {
-        // Incrementați numărul de greșeli și afișați rezultatul în interfața utilizatorului
+      if (!isAlreadyGuessed) {
         maxGreseli--;
-        paragraf.textContent = `Ai gresit! Mai ai ${maxGreseli} incercari.`
-
-        if(maxGreseli === 0) {
-          mesaj.textContent = `Ai pierdut! Cuvantul era ${cuvantAles}`;
-          letters.forEach(element => {
-            element.setAttribute("disabled", true);
-          });
-          mesaj.style.display = "block";
-          onReset()
-          photo.querySelectorAll('[id]').forEach(x => x.style.display="block");
-        } 
-
-        document.querySelector("#id" + maxGreseli).style.display = 'block';
       }
-    });
+    } else {
+      // Incrementați numărul de greșeli și afișați rezultatul în interfața utilizatorului
+      maxGreseli--;
+      paragraf.textContent = `Ai greșit! Mai ai ${maxGreseli} încercări.`;
+
+      if(maxGreseli === 0) {
+        mesaj.textContent = `Ai pierdut! Cuvantul era ${cuvantAles}`;
+        letters.forEach(element => {
+          element.setAttribute("disabled", true);
+        });
+        mesaj.style.display = "block";
+        onReset()
+        photo.querySelectorAll('[id]').forEach(x => x.style.display="block");
+      } 
+
+      document.querySelector("#id" + maxGreseli).style.display = 'block';
+    }
   });
+});
 
 
-// Alegere cuvant random
-let cuvantAles = cuvinte[Math.floor(Math.random() * cuvinte.length)];
-cuvantAles = cuvantAles.toUpperCase();
-
-// Inlocuire litera cu span ce contine ( _ ) 
-arataLitera = cuvantAles.replace(/./g, '<span class="dash">_</span>');
+// Inlocuire litera cu span ce conține ( _ ) 
+const arataLitera = cuvantAles.replace(/./g, '<span class="dash">_</span>');
 
 // Afisare
 inputSection.innerHTML = arataLitera;
 
-document.addEventListener('DOMContentLoaded', onLoad); //resetare pagina la fiecare deschidere in browser
- 
+document.addEventListener('DOMContentLoaded', onLoad); // Resetare pagina la fiecare deschidere în browser
 
-
- function onLoad(){
-    onReset();
-    btn.addEventListener('click', onReset);//refresh pagina la apasare pe buton
+// Resetare desen la refresh pagina
+function onReset() {
+  photo.querySelectorAll('[id^="id"]').forEach(x => (x.style.display = "none"));
 }
 
-//Resetare desen la refresh pagina
-function onReset(){
-    photo.querySelectorAll('[id]')
-        .forEach(x => x.style.display="none");
+function onLoad() {
+  onReset();
+  btn.addEventListener("click", onReset); // Refresh pagina la apăsare pe buton
 }
 
